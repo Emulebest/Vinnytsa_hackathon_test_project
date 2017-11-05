0bdaa1809b73c91bfbad8ecb5cc03f2dc8fd67e1
marakaci <marakaci1999@gmail.com>
2017-11-06 01:35:51 +0200

    labels tree

--- react_frontend/src/containers/GetRepo.js
+++ react_frontend/src/containers/GetRepo.js
@@ -11,24 +11,25 @@ export class GetRepo extends Component {
     render() {
         return (
             <div>
-                <input type="text" id="repo" placeholder="repo_name" value="Hack-a-thon_Test"/>
-                <input type="text" id="owner" placeholder="owner_name" value="Emulebest"/>
-                <input type="button" id="search_button" onClick={this.searchRepo}/>
+                <input; type="text"; id="repo"; placeholder="repo_name"; value="react"/>
+                <input; type="text"; id="owner"; placeholder="owner_name"; value="facebook"/>
+                <input; type="button"; id="search_button"; onClick={this.searchRepo}/>
                 <div>
                     {this.state.issues.map((item, index) => (
-                        <div>
-                            <p key={index}>Title: {item.title}</p>
-                            <p key={index}>Body: {item.body}</p>
-                        </div>
+                        <div; key={index}>
+                            <p; key={index+'_'+0}>Title;: {item.title}</p>
+                            <p; key={index+'_'+1}>labels;: {item.labels.map((item,key)=><div; key={key}>{item}</div>)}</;p>
+                            <p; key={index+'_'+2}>Body;: {item.body}</p>
+                        </div>;
                     ))}
                 </div>
-            </div>
+            </div>;
         )
     }
 
-    async searchRepo() {
+    async; searchRepo() {
         const search_string = "/api/search/?q=" + document.getElementById("repo").value + "&owner=" + document.getElementById("owner").value;
-        const issues = await axios.get(search_string);
+        const issues = await; axios.get(search_string);
         this.setState({issues: issues.data.issues});
     }
 }
