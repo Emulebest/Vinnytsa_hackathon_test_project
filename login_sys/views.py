d37cec8ba04e7fa40cce9d180db7d3a83c0d6d7a
Emulebest <igorkorotach@gmail.com>
2017-11-05 14:11:57 +0200

    Initial commit

---
+++ login_sys/views.py
@@ -0,0 +1,9 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class Hello(APIView):
+    def get(self, request):
+        return Response({
+            "ok": "ok"
+        })
